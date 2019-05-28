4c7b2ab14228468143c26ac30f5889634b4210ea
Cárdenas, Mario Alberto <marioalberto.cardenas@thermofisher.com>
2019-05-27 17:08:52 -0700

    first commit

---
+++ src/test/java/com/mac/algorithms/codility/iterations/BinaryGapTest.java
@@ -0,0 +1,22 @@
+package com.mac.algorithms.codility.iterations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BinaryGapTest {
+
+    BinaryGap binaryGap;
+
+    @BeforeEach
+    void init() {
+        binaryGap = new BinaryGap();
+    }
+
+    @Test
+    public void testMethid(){
+        int result = binaryGap.solution(1041);
+        Assertions.assertEquals(5, result);
+    }
+
+}
