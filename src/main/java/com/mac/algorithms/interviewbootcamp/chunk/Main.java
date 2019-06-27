d707b5c50aa5662fdfff52a18bbf8d31f5d5f4c5
Cárdenas, Mario Alberto <marioalberto.cardenas@thermofisher.com>
2019-06-27 15:45:56 -0700

    interviewbootcamp -> chunk: updated

--- src/main/java/com/mac/algorithms/interviewbootcamp/chunk/Main.java
+++ src/main/java/com/mac/algorithms/interviewbootcamp/chunk/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int array[] = {1, 2, 3, 4, 5};
 //        int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
-        int[][] result = solutionTwo(array, 3);
+        int[][] result = solutionOne(array, 3);
 
         for (int[] a : result) {
             System.out.println(Arrays.toString(a));
@@ -28,27 +28,27 @@ public class Main {
         return result;
     }
 
-//    private static int[][] solutionTwo(int[] array, int size) {
-////        int chunk = (array.length % size) == 0 ? array.length / size : array.length / size + 1;
-//        int chunk = (int)Math.ceil((float) array.length/size);
-//
-//        int[][] result = new int[chunk][size];
-//
-//        int indexA = 0;
-//        int indexB = 0;
-//
-//        for (int i = 0; i < array.length; i++) {
-//            result[indexA][indexB] = array[i];
-//            indexB++;
-//            if (indexB % size == 0) {
-//                indexA++;
-//                indexB = 0;
-//            }
-//
-//        }
-//
-//        return result;
-//    }
+    private static int[][] solutionTwo(int[] array, int size) {
+//        int chunk = (array.length % size) == 0 ? array.length / size : array.length / size + 1;
+        int chunk = (int)Math.ceil((float) array.length/size);
+
+        int[][] result = new int[chunk][size];
+
+        int indexA = 0;
+        int indexB = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            result[indexA][indexB] = array[i];
+            indexB++;
+            if (indexB % size == 0) {
+                indexA++;
+                indexB = 0;
+            }
+
+        }
+
+        return result;
+    }
 
 
 }
